6970a50a13e2dd4cc497bfadab716fc78f1122be
David Thompson <davthomp@redhat.com>
2025-08-27 15:30:01 -0400

    Remove chai
    Recent versions of chai use ES Modules,
    which makes it hard to use.
    
    Use nodejs' built-in assertion library instead,
    which should be sufficient for this project.
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
-import { expect } from 'chai';
+import * as assert from 'assert/strict';
 
 describe('VS Code extension tests', () => {
 
   it('should be present', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    expect(vscode.extensions.getExtension('redhat.vscode-microprofile')).to.be.ok;
+    assert.ok(vscode.extensions.getExtension('redhat.vscode-microprofile'));
   });
 });
--- src/test/suite/languageServer/documentSelectorPlugin.test.ts
+++ src/test/suite/languageServer/documentSelectorPlugin.test.ts
@@ -15,7 +15,7 @@
  */
 import * as vscode from "vscode";
 import * as plugin from "../../../languageServer/plugin";
-import { expect } from "chai";
+import * as assert from "assert";
 import { MicroProfileContribution } from "../../../languageServer/plugin";
 import { TextDocumentFilter, DocumentSelector } from "vscode-languageclient";
 
@@ -28,112 +28,117 @@ describe("Document selector collection from language server plugins", () => {
 
   it ('Should collect document selector when all keys exist', () => {
     const selector: DocumentSelector = collectDocumentSelectors([{ scheme: "file", language: "quarkus-properties", pattern: "**/*.properties" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["scheme", "language", "pattern"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
-    expect((selector[0] as TextDocumentFilter).language).to.equal("quarkus-properties");
-    expect((selector[0] as TextDocumentFilter).pattern).to.equal("**/*.properties");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.ok(Object.keys(selector[0]).includes("language"));
+    assert.ok(Object.keys(selector[0]).includes("pattern"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
+    assert.equal((selector[0] as TextDocumentFilter).language, "quarkus-properties");
+    assert.equal((selector[0] as TextDocumentFilter).pattern, "**/*.properties");
   });
 
   it ('Should collect all document selector when two keys exist', () => {
     let selector: DocumentSelector = collectDocumentSelectors([{ scheme: "file", language: "quarkus-properties" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["scheme", "language"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
-    expect((selector[0] as TextDocumentFilter).language).to.equal("quarkus-properties");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.ok(Object.keys(selector[0]).includes("language"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
+    assert.equal((selector[0] as TextDocumentFilter).language, "quarkus-properties");
 
     selector = collectDocumentSelectors([{ language: "quarkus-properties", pattern: "**/*.properties" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["language", "pattern"]);
-    expect((selector[0] as TextDocumentFilter).language).to.equal("quarkus-properties");
-    expect((selector[0] as TextDocumentFilter).pattern).to.equal("**/*.properties");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("language"));
+    assert.ok(Object.keys(selector[0]).includes("pattern"));
+    assert.equal((selector[0] as TextDocumentFilter).language, "quarkus-properties");
+    assert.equal((selector[0] as TextDocumentFilter).pattern, "**/*.properties");
 
     selector = collectDocumentSelectors([{ pattern: "**/*.properties", scheme: "file" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["pattern", "scheme"]);
-    expect((selector[0] as TextDocumentFilter).pattern).to.equal("**/*.properties");
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("pattern"));
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.equal((selector[0] as TextDocumentFilter).pattern, "**/*.properties");
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
   });
 
   it ('Should collect document selector when one key exist', () => {
     let selector: DocumentSelector = collectDocumentSelectors([{ scheme: "file" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["scheme"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
 
     selector = collectDocumentSelectors([{ language: "quarkus-properties" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["language"]);
-    expect((selector[0] as TextDocumentFilter).language).to.equal("quarkus-properties");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("language"));
+    assert.equal((selector[0] as TextDocumentFilter).language, "quarkus-properties");
 
     selector = collectDocumentSelectors([{ pattern: "**/*.properties" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["pattern"]);
-    expect((selector[0] as TextDocumentFilter).pattern).to.equal("**/*.properties");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("pattern"));
+    assert.equal((selector[0] as TextDocumentFilter).pattern, "**/*.properties");
   });
 
   it ('Should collect document selector when a valid key and an invalid key exists', () => {
     // valid, but the "invalid" key is ignored
     let selector: DocumentSelector = collectDocumentSelectors([{ scheme: "file", invalid: "file" }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["scheme"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
 
     // valid, but the "language" key is ignored since the value has the wrong type
     selector = collectDocumentSelectors([{ scheme: "file", language: 12 }]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.have.all.keys(["scheme"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
+    assert.equal(selector.length, 1);
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
   });
 
   it ('Should collect document selector strings', () => {
     const selector: DocumentSelector = collectDocumentSelectors(["document-selector-string"]);
-    expect(selector).to.have.length(1);
-    expect(selector[0]).to.be.a('string');
-    expect(selector[0]).to.equal("document-selector-string");
+    assert.equal(selector.length, 1);
+    assert.equal(typeof selector[0], "string");
+    assert.equal(selector[0], "document-selector-string")
   });
 
   it ('Should not collect document selector when there are no correct keys', () => {
     let selector: DocumentSelector = collectDocumentSelectors([{ invalid: "file" }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{}]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
   });
 
   it ('Should not collect document selector when containing invalid types', () => {
     let selector: DocumentSelector = collectDocumentSelectors([12]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([/test/]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([true]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
   });
 
   it ('Should not collect document selector when all keys have incorrect types', () => {
 
     let selector: DocumentSelector = collectDocumentSelectors([{ scheme: 12 }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{ scheme: { key: "value" } }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{ language: 12 }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{ language: { key: "value" } }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{ pattern: 12 }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
 
     selector = collectDocumentSelectors([{ pattern: { key: "value" } }]);
-    expect(selector).to.have.length(0);
+    assert.equal(selector.length, 0);
   });
 
   it ('Should collect multiple document selectors', () => {
@@ -145,18 +150,20 @@ describe("Document selector collection from language server plugins", () => {
       12 // invalid
     ]);
 
-    expect(selector).to.have.length(3);
+    assert.equal(selector.length, 3);
 
-    expect(selector[0]).to.have.all.keys(["scheme", "language", "pattern"]);
-    expect((selector[0] as TextDocumentFilter).scheme).to.equal("file");
-    expect((selector[0] as TextDocumentFilter).language).to.equal("quarkus-properties");
-    expect((selector[0] as TextDocumentFilter).pattern).to.equal("**/*.properties");
+    assert.ok(Object.keys(selector[0]).includes("scheme"));
+    assert.ok(Object.keys(selector[0]).includes("language"));
+    assert.ok(Object.keys(selector[0]).includes("pattern"));
+    assert.equal((selector[0] as TextDocumentFilter).scheme, "file");
+    assert.equal((selector[0] as TextDocumentFilter).language, "quarkus-properties");
+    assert.equal((selector[0] as TextDocumentFilter).pattern, "**/*.properties");
 
-    expect(selector[1]).to.have.all.keys(["language"]);
-    expect((selector[1] as TextDocumentFilter).language).to.equal("my-properties");
+    assert.ok(Object.keys(selector[1]).includes("language"));
+    assert.equal((selector[1] as TextDocumentFilter).language, "my-properties");
 
-    expect(selector[2]).to.be.a('string');
-    expect(selector[2]).to.equal("document-selector-string");
+    assert.equal(typeof selector[2], "string");
+    assert.equal(selector[2], "document-selector-string");
   });
 
   /**
@@ -186,7 +193,7 @@ describe("Document selector collection from language server plugins", () => {
     };
 
     const contribution: MicroProfileContribution[] = plugin.collectMicroProfileJavaExtensions([ fakePlugin ]);
-    expect(contribution).to.have.length(1);
+    assert.equal(contribution.length, 1);
 
     return contribution[0].documentSelector;
   }
